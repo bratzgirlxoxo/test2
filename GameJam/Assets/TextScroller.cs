8758aea536b0e4a833c901c624b1093fd6990fa6
jimistine <jrstine@me.com>
2017-12-11 11:52:51 -0500

    Intro scene first pass,
    intro text, scrolling script, jingle SFX, and snow particle system.

---
+++ GameJam/Assets/TextScroller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScroller : MonoBehaviour
+{
+
+	public float text_x;
+	public float text_y;
+	public float text_z;
+
+	public float scrollSpeed;
+	public float stopSpot;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		text_x = gameObject.transform.position.x;
+		text_y = gameObject.transform.position.y;
+		text_z = gameObject.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (text_y <= stopSpot)
+		{
+			text_y += scrollSpeed;
+		}
+		
+		gameObject.transform.position = new Vector3(text_x,text_y,text_z);
+	}
+}
