9b4d14782c0b0969cefffe24eddd281d9fafc506
Unknown <plp248@nyu.edu>
2017-12-13 11:14:38 -0500

    grinch movent & cam

---
+++ GameJam/Assets/scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+	public float speed = 5;
+
+	void Update () 
+	{
+		NormalMove();	
+	}
+
+
+	void NormalMove()
+	{
+		// right
+		if (Input.GetAxisRaw("Horizontal") > 0)
+		{
+			transform.Translate(Vector3.right * speed * Time.deltaTime);
+		}
+		// left
+		if (Input.GetAxisRaw("Horizontal") < 0)
+		{
+			transform.Translate(Vector3.left * speed * Time.deltaTime);
+		}
+		// up
+		if (Input.GetAxisRaw("Vertical") > 0)
+		{
+			transform.Translate(Vector3.up * speed * Time.deltaTime);
+		}
+		// down
+		if (Input.GetAxisRaw("Vertical") < 0)
+		{
+			transform.Translate(Vector3.down * speed * Time.deltaTime);
+		}
+
+	}
+}
